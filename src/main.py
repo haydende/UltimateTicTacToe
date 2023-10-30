38584701bb62ed75e06f0cbb50827403f784a001
Hayden Eastwell <haydeneastwell@outlook.com>
2023-10-30 21:33:24 +0000

    Created TicTacToeBoard, with internal Divs and Buttons

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import justpy as jp
+import TicTacToeBoard.TicTacToeBoard as ttb
+
+
+def main():
+    board = ttb.TicTacToeBoard()
+    jp.justpy(board.start)
+
+
+if __name__ == "__main__":
+    main()

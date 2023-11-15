7d4ee73904e73072ebb7c0892d4f9a2956a0a462
Hayden Eastwell <haydeneastwell@outlook.com>
2023-11-15 08:49:32 +0000

    Refactored how GUI Board is created, instead using NiceGUI

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-import justpy as jp
-import TicTacToeBoard.TicTacToeBoard as ttb
+from src.component import TicTacToeBoard as ttb
 
 
 def main():
     board = ttb.TicTacToeBoard()
-    jp.justpy(board.start)
+    ttb.start()
 
 
-if __name__ == "__main__":
-    main()
+main()
